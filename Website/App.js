2d4b70b7c478f9f633c5de7e091e90f628e35173
Smriti Vipin Madangarli <smritivipin.madangarli2021@vitstudent.ac.in>
2023-10-02 15:23:36 +0530

    Create link to flower page

---
+++ Website/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+//import LoginPage1 from './login.jsx';
+import LoginPage2 from './signup.jsx';
+
+
+export default function page(){
+    return (
+        <div>
+            {/* <LoginPage1/> */}
+            <LoginPage2/>
+
+        </div>
+    );
+}
