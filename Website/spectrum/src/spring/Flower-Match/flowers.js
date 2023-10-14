8aad8d2414e02b00f7cf9f8e660d4572c8ace716
GauriSP <gpisharady@gmail.com>
2023-10-14 12:42:54 +0530

    added remaining flowers

--- Website/spectrum/src/spring/Flower-Match/flowers.js
+++ Website/spectrum/src/spring/Flower-Match/flowers.js
@@ -2,11 +2,15 @@ import Bouganvilla from './Flowers_pic/bouganvilla.png';
 import Daffodil from './Flowers_pic/daffodil.png';
 import Daisy from './Flowers_pic/daisy.jpg';
 import Hibiscus from './Flowers_pic/hibiscus.png';
-import Hyacinth from './Flowers_pic/hyacinth.png'
+import Hyacinth from './Flowers_pic/hyacinth.png';
 import Rose from './Flowers_pic/rose.png';
-import Sunflower from './Flowers_pic/sunflower.png'
-
+import Sunflower from './Flowers_pic/sunflower.png';
+import Bluebell from './Flowers_pic/bluebell.png';
+import Tulip from './Flowers_pic/tulip.png';
+import Orchid from './Flowers_pic/orchid.png';
+import Cherry from './Flowers_pic/cherry.jpg';
+import Lily from './Flowers_pic/lily.jpeg';
 
 export const images = [
-	Bouganvilla, Daffodil, Daisy, Hibiscus, Hyacinth, Rose, Sunflower
+	Bouganvilla, Daffodil, Daisy, Hibiscus, Hyacinth, Rose, Sunflower, Bluebell, Tulip, Orchid, Cherry, Lily
 ]
