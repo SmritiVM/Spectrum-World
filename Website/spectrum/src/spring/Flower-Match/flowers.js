af48c960c021a33c86f0619543b8d3a147fab370
Smriti Vipin Madangarli <smritivipin.madangarli2021@vitstudent.ac.in>
2023-10-14 11:47:07 +0530

    Fixed memory match images

--- Website/spectrum/src/spring/Flower-Match/flowers.js
+++ Website/spectrum/src/spring/Flower-Match/flowers.js
@@ -1,11 +1,12 @@
+import Bouganvilla from './Flowers_pic/bouganvilla.png';
+import Daffodil from './Flowers_pic/daffodil.png';
+import Daisy from './Flowers_pic/daisy.jpg';
+import Hibiscus from './Flowers_pic/hibiscus.png';
+import Hyacinth from './Flowers_pic/hyacinth.png'
+import Rose from './Flowers_pic/rose.png';
+import Sunflower from './Flowers_pic/sunflower.png'
+
+
 export const images = [
-    "Flower-Match/Flowers_pic/daisy.jpg",
-	"C:/Users/gpish/Documents/GitHub/Spectrum-World/Website/spectrum/src/spring/Flower-Match/Flowers_pic/hibiscus.png",
-	"https://progitek.no/privat/bp/wp-content/uploads/2021/09/pexels-magda-ehlers-451230.jpg",
-	"https://progitek.no/privat/bp/wp-content/uploads/2021/09/pexels-funny-foxy-pride-6244506.jpg",
-	"https://progitek.no/privat/bp/wp-content/uploads/2021/09/pexels-kat-wood-7113831.jpg",
-	"https://progitek.no/privat/bp/wp-content/uploads/2021/09/pexels-anthony-133356.jpg",
-	"https://progitek.no/privat/bp/wp-content/uploads/2021/09/pexels-kendra-coupland-2642167.jpg",
-	"https://progitek.no/privat/bp/wp-content/uploads/2021/09/pexels-rachel-claire-5490707.jpg",
-	"https://progitek.no/privat/bp/wp-content/uploads/2021/09/pexels-cottonbro-4910769.jpg", 
+	Bouganvilla, Daffodil, Daisy, Hibiscus, Hyacinth, Rose, Sunflower
 ]
