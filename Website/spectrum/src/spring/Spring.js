0e1c1ad895605b6758e172cb24a7a1e50496d348
Smriti Vipin Madangarli <smritivipin.madangarli2021@vitstudent.ac.in>
2023-10-17 11:11:57 +0530

    Added bootstrap module

--- Website/spectrum/src/spring/Spring.js
+++ Website/spectrum/src/spring/Spring.js
@@ -1,14 +1,20 @@
-import React from "react";
+import {React, useState} from "react";
 import { useNavigate } from 'react-router-dom';
+import {Container} from "react-bootstrap";
+
+import Back1 from "./assets/back_1.png";
+
+import "./Spring.css";
 
 export default function Spring(){
     const navigate = useNavigate();
+    const [background, setBackground] = useState(`url(${Back1})`);
     
     return(
-        <div>
+        <div className = "container" style={{"backgroundImage":background}}>
             <h1>This is spring page</h1>
             <hr/>
-            <button onClick = {() => navigate("/flower")}>Go to flower match game</button>
+            <button className = "next" onClick = {() => navigate("/flower")}>Go to flower match game</button>
         </div>
     )
 }
