bb741ac033a6c55e06dd4fb8cfa24ba128b67f03
Smriti Vipin Madangarli <smritivipin.madangarli2021@vitstudent.ac.in>
2023-11-04 09:31:07 +0530

    Added new routes

--- Website/spectrum/src/App.js
+++ Website/spectrum/src/App.js
@@ -4,11 +4,16 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // import LoginPage1 from './login.jsx';
 // import LoginPage2 from './signup.jsx';
 import Home from './Home.js';
+
+//Spring imports
 import Spring from './spring/Spring.js';
 import FlowerMatch from './spring/Flower-Match/Flower-Match.js';
 import Pathway from './spring/Pathway-Game/pathway.js';
 import BeeGame from './spring/Bee-Game/BeeGame.js';
+import SpringEnd from './spring/SpringEnd.js';
 
+//Summer imports
+import Summer from './summer/Summer.js';
 
 export default function page(){
     return (
@@ -19,10 +24,16 @@ export default function page(){
         <Router>
             <Routes>
                 <Route path = "/" element = {<Home/>}/>
+
+                {/* Spring Routes */}
                 <Route path = "/spring" element = {<Spring/>}/>
                 <Route path = "/flower" element = {<FlowerMatch/>}/>
                 <Route path = "/pathway" element = {<Pathway/>}/>
                 <Route path = "/bee" element = {<BeeGame/>}/>
+                <Route path = "/springend" element = {<SpringEnd/>}/>
+
+                {/* Summer Routes */}
+                <Route path = "/summer" element = {<Summer/>}/>
             </Routes>
         </Router>
     );
