eaa1117561e395a473fdd94d3d68c4fa20703117
GauriSP <gpisharady@gmail.com>
2023-11-11 19:12:36 +0530

    updates to autumn apple game
    Added a bg image, few conditions to update apple and score to end game

--- Website/spectrum/src/autumn/catchApple.js
+++ Website/spectrum/src/autumn/catchApple.js
@@ -1,24 +1,27 @@
 import React, { useState, useEffect } from 'react';
+import Basket from './basket.png';
+import './catchApple.css';
 
 const getRandomPosition = () => ({
-  top: 0,
-  left: Math.random() * window.innerWidth,
+  top: 130,
+  left: 30 + Math.random() * (window.innerWidth - 60)
 });
 
 const CatchTheAppleGame = () => {
   const [applePosition, setApplePosition] = useState(getRandomPosition());
   const [basketPosition, setBasketPosition] = useState({
-    top: window.innerHeight - 50,
-    left: window.innerWidth / 2 - 25,
+    top: window.innerHeight - 100,
+    left: window.innerWidth / 2 - 50,
   });
+
   const [score, setScore] = useState(0);
 
   useEffect(() => {
     const handleKeyPress = (e) => {
-      if (e.key === 'ArrowLeft' && basketPosition.left > 0) {
-        setBasketPosition({ ...basketPosition, left: basketPosition.left - 10 });
-      } else if (e.key === 'ArrowRight' && basketPosition.left < window.innerWidth - 50) {
-        setBasketPosition({ ...basketPosition, left: basketPosition.left + 10 });
+      if (e.key === 'ArrowLeft' && basketPosition.left > 30) {
+        setBasketPosition({ ...basketPosition, left: basketPosition.left - 30 });
+      } else if (e.key === 'ArrowRight' && basketPosition.left < window.innerWidth - 130) {
+        setBasketPosition({ ...basketPosition, left: basketPosition.left + 30 });
       }
     };
 
@@ -27,18 +30,27 @@ const CatchTheAppleGame = () => {
     };
 
     const moveApple = () => {
-      setApplePosition((prevPosition) => ({
-        top: prevPosition.top + 5, // Adjust the falling speed as needed
-        left: prevPosition.left,
-      }));
+      setApplePosition((prevPosition) => {
+        const newPosition = {
+          top: prevPosition.top + 10, // Adjust the falling speed as needed
+          left: prevPosition.left,
+        };
+
+        if (newPosition.top + 50 >= basketPosition.top + 100) {
+          updateApple(); // Update the apple's position if it falls beneath the basket
+        }
+    
+        return newPosition;
+      });
     };
+    
 
     const checkCatch = () => {
       if (
         applePosition.top > basketPosition.top &&
-        applePosition.top < basketPosition.top + 50 &&
+        applePosition.top < basketPosition.top + 100 &&
         applePosition.left > basketPosition.left &&
-        applePosition.left < basketPosition.left + 50
+        applePosition.left < basketPosition.left + 100
       ) {
         setScore(score + 1);
         updateApple();
@@ -46,10 +58,17 @@ const CatchTheAppleGame = () => {
     };
 
     window.addEventListener('keydown', handleKeyPress);
+
     const basketInterval = setInterval(() => {
       moveApple();
       checkCatch();
-    }, 100); // Adjust the interval based on the falling speed
+
+      if (score === 3) {       //CHANGE SCORE TO MOVE ON
+        clearInterval(basketInterval);
+        clearInterval(appleInterval);
+        alert('Game Over! You scored 10 points.');
+      }
+    }, 70); // Adjust the interval based on the falling speed
 
     const appleInterval = setInterval(updateApple, 5000); // Adjust the interval for generating new apples
 
@@ -61,8 +80,9 @@ const CatchTheAppleGame = () => {
   }, [score, applePosition, basketPosition]);
 
   return (
-    <div>
-      <h1>Catch the Apple Game</h1>
+    <div className="applegame" >
+    <div className="game-container">
+    <h1>Catch the Apple Game</h1>
       <p>Score: {score}</p>
       <div
         style={{
@@ -79,12 +99,16 @@ const CatchTheAppleGame = () => {
           position: 'absolute',
           top: basketPosition.top,
           left: basketPosition.left,
-          width: '50px',
-          height: '50px',
-          backgroundColor: 'blue',
+          width: '100px',
+          height: '100px',
+          backgroundImage: `url(${Basket})`,
+          backgroundSize: 'cover'
+          //backgroundColor: 'blue',
         }}
       />
     </div>
+    </div>
+
   );
 };
 
