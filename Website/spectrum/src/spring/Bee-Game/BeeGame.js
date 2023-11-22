055748932a0a98d739a73a51efd2e6f49ed06b4d
Smriti Vipin Madangarli <smritivipin.madangarli2021@vitstudent.ac.in>
2023-11-22 10:08:35 +0530

    Added instructions before flower game

--- Website/spectrum/src/spring/Bee-Game/BeeGame.js
+++ Website/spectrum/src/spring/Bee-Game/BeeGame.js
@@ -56,6 +56,28 @@ export default function BeeGame(){
     const [avatar, setAvatar] = useState()
     const [instructions, setInstructions] = useState()
 
+    const goToFlower = () => {
+        setAvatar()
+        setInstructions(
+            <Container fluid>
+                <Row>
+                    <p style={{fontSize:"2vw"}}>Let's help Buzz collect flowers, shall we? <br/><br/>
+                    Flip 2 cards at a time to find the hidden flowers.
+                     If the flowers are the same, it is a match and Buzz gets to keep the flower! Match all flowers to win the game😊</p>
+                </Row>
+                <Row>
+                    <center>
+                        <Button  className="helpChoice" onClick={() => navigate("/spring/flower")}>
+                            Start
+                        </Button>
+                    </center>
+                </Row>
+            </Container>
+        )
+        setDialogue()
+        setNext()
+
+    }
     const goToBuzz3 = () => {
         setAvatar(<AvatarIcon alt = "Bee" src = {Bee} style={{"scale":"5"}} />)
         setDialogue(["All right! I'll see you soon! Bzz...bz.."])
@@ -63,12 +85,12 @@ export default function BeeGame(){
 
     }
 
-    const goToFlower = () => {
+    const goToBuzz4 = () => {
         setInstructions()
         setAvatar(<AvatarIcon alt = "Bee" src = {Bee} style={{"scale":"5"}} />)
         setDialogue(["Thank you! Off we go, buzzing to infinity and bee-yond!", "*Click on the arrow to proceed*"])
         setNext(
-            <NextButton onClick = {() => navigate("/spring/flower")}><NextIcon/></NextButton>
+            <NextButton onClick = {goToFlower}><NextIcon/></NextButton>
         )
 
     }
@@ -80,7 +102,7 @@ export default function BeeGame(){
                 </Row>
                 <Row>
                     <Col md = {6}>
-                        <Button  className="helpChoice" onClick={goToFlower}>
+                        <Button  className="helpChoice" onClick={goToBuzz4}>
                             Sure I'd love to!
                         </Button>
                     </Col>
